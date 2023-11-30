3a2a98be9a6dd7abe6a327f38a9bf0f4dab59973
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2023-11-30 15:38:47 +0800

    feat/category_profile/not_finish

--- frontend/src/app/customer/[id]/page.tsx
+++ frontend/src/app/customer/[id]/page.tsx
@@ -2,7 +2,7 @@
 import { useParams } from 'next/navigation';
 import RestaurantCard from '@/components/StoreInfo';
 import StoreMenu from '@/components/StoreMenu';
-import { restaurantData } from '@/app/dbTemplate/cardData';
+import { restaurantData, categoryDetailData } from '@/app/dbTemplate/cardData';
 
 const RestaurantPage = () => {
     const { id } = useParams();
@@ -11,7 +11,7 @@ const RestaurantPage = () => {
     // Use 'id' to fetch data or for other purposes
     return (
         <>
-            <RestaurantCard  name={name} address={restaurantData[0].address} starNumber={restaurantData[0].starNumber} imageSrc={restaurantData[0].uri} likes={true} />
+            <RestaurantCard  name={restaurantData[0].name} address={restaurantData[0].address} starNumber={restaurantData[0].starNumber} imageSrc={restaurantData[0].uri} likes={true} />
             <StoreMenu menu={restaurantData[0].menu} />
         </>
     );
