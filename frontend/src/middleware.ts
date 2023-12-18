022803b1af9420563435ddfe6914bc0dc7316693
jwtsao <jwtsao1999@gmail.com>
2023-12-18 21:27:19 +0800

    middleware.ts modification for avoiding ALB health check failed

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,12 +1,17 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 const publicRoutes = ["/"];
+const ignoredRoutes = [
+  "/((?!api|trpc))(_next.*|.+\.[\w]+$)", 
+  "/foody-backend-ALB-1197695098.us-east-1.elb.amazonaws.com/clerk/register"
+];
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: publicRoutes,
+  ignoredRoutes: ignoredRoutes,
 });
 
 export const config = {
