2f3fd53c1a02854c17d58a0f90c4f4419dcfe870
tsailawrence <tsailawrence812@gmail.com>
2023-12-21 07:32:46 +0800

    update

--- qa/tests/auth.setup.ts
+++ qa/tests/auth.setup.ts
@@ -13,16 +13,16 @@ setup('authenticate as customer', async ({ page }) => {
   await page.goto(baseURL);
   console.log(page.url());
   await page.locator('span:text("Get Started")').click();
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(8000);
   await page.screenshot({ path: 'screenshot.png' });
   console.log(page.url());
   await page.locator('#identifier-field').fill(customerAccount);
   await page.keyboard.press('Enter');
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(8000);
   console.log(page.url());
   await page.locator('#password-field').fill(customerPassword);
   await page.keyboard.press('Enter');
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(8000);
   // Wait until the page receives the cookies.
   // Alternatively, you can wait until the page reaches a state where all cookies are set.
   console.log(page.url());
@@ -39,15 +39,15 @@ setup('authenticate as merchant', async ({ page }) => {
   await page.goto(baseURL);
   console.log(page.url());
   await page.locator('span:text("Get Started")').click();
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(8000);
   console.log(page.url());
   await page.locator('#identifier-field').fill(merchantAccount);
   await page.keyboard.press('Enter');
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(8000);
   console.log(page.url());
   await page.locator('#password-field').fill(merchantPassword);
   await page.keyboard.press('Enter');
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(8000);
   // Wait until the page receives the cookies.
   // Alternatively, you can wait until the page reaches a state where all cookies are set.
   console.log(page.url());
