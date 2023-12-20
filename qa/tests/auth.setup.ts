fa8e34cf004b55c0d9482d045b964cbca124cbd2
tsailawrence <tsailawrence812@gmail.com>
2023-12-21 06:15:53 +0800

    test

--- qa/tests/auth.setup.ts
+++ qa/tests/auth.setup.ts
@@ -10,14 +10,15 @@ const customerFile = 'playwright/.auth/customer.json';
 setup('authenticate as customer', async ({ page }) => {
   // Perform authentication steps. Replace these actions with your own.
   await page.goto(baseURL);
+  console.log(page.url());
   await page.locator('span:text("Get Started")').click();
   await page.locator('#identifier-field').fill(customerAccount);
   await page.keyboard.press('Enter');
   await page.waitForTimeout(2000);
+  console.log(page.url());
   await page.locator('#password-field').fill(customerPassword);
   await page.keyboard.press('Enter');
-  await page.waitForTimeout(5000);
-  await page.waitForLoadState('networkidle');
+  await page.waitForTimeout(8000);
   // Wait until the page receives the cookies.
   // Alternatively, you can wait until the page reaches a state where all cookies are set.
   console.log(page.url());
@@ -31,14 +32,15 @@ const merchantFile = 'playwright/.auth/merchant.json';
 setup('authenticate as merchant', async ({ page }) => {
   // Perform authentication steps. Replace these actions with your own.
   await page.goto(baseURL);
+  console.log(page.url());
   await page.locator('span:text("Get Started")').click();
   await page.locator('#identifier-field').fill(merchantAccount);
   await page.keyboard.press('Enter');
   await page.waitForTimeout(2000);
+  console.log(page.url());
   await page.locator('#password-field').fill(merchantPassword);
   await page.keyboard.press('Enter');
-  await page.waitForTimeout(5000);
-  await page.waitForLoadState('networkidle');
+  await page.waitForTimeout(8000);
   // Wait until the page receives the cookies.
   // Alternatively, you can wait until the page reaches a state where all cookies are set.
   console.log(page.url());
