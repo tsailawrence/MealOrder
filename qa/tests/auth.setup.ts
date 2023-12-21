79514f5918401929454ae774de2bfbb7845390e5
tsailawrence <tsailawrence812@gmail.com>
2023-12-21 10:39:07 +0800

    update

--- qa/tests/auth.setup.ts
+++ qa/tests/auth.setup.ts
@@ -1,4 +1,4 @@
-import { test as setup } from '@playwright/test';
+import { test as setup, expect } from '@playwright/test';
 
 const baseURL = 'http://localhost:3000';
 const customerAccount = 'customer+clerk_test@test.com';
@@ -12,10 +12,10 @@ setup('authenticate as customer', async ({ page }) => {
   console.log('customer test');
   await page.goto(baseURL);
   console.log(page.url());
-  await page.locator('span:text("Get Started")').click();
+  await page.locator('button:text("Get Started")').click();
   await page.waitForTimeout(1000);
-  // await page.screenshot({ path: path.join(screenshotsDir, 'screenshot1.png') });
   console.log(page.url());
+  expect(page.getByText('Continue with LINE')).toBeVisible();
   await page.locator('#identifier-field').fill(customerAccount);
   await page.keyboard.press('Enter');
   await page.waitForTimeout(1000);
@@ -23,8 +23,6 @@ setup('authenticate as customer', async ({ page }) => {
   await page.locator('#password-field').fill(customerPassword);
   await page.keyboard.press('Enter');
   await page.waitForTimeout(3000);
-  // Wait until the page receives the cookies.
-  // Alternatively, you can wait until the page reaches a state where all cookies are set.
   console.log(page.url());
   // await expect(page.locator('#logo')).toBeVisible();
   await page.waitForLoadState('networkidle');
@@ -39,10 +37,10 @@ setup('authenticate as merchant', async ({ page }) => {
   console.log('merchant test');
   await page.goto(baseURL);
   console.log(page.url());
-  await page.locator('span:text("Get Started")').click();
+  await page.locator('button:text("Get Started")').click();
   await page.waitForTimeout(1000);
   console.log(page.url());
-  // await page.screenshot({ path: path.join(screenshotsDir, 'screenshot2.png') });
+  expect(page.getByText('Continue with LINE')).toBeVisible();
   await page.locator('#identifier-field').fill(merchantAccount);
   await page.keyboard.press('Enter');
   await page.waitForTimeout(1000);
@@ -50,8 +48,6 @@ setup('authenticate as merchant', async ({ page }) => {
   await page.locator('#password-field').fill(merchantPassword);
   await page.keyboard.press('Enter');
   await page.waitForTimeout(3000);
-  // Wait until the page receives the cookies.
-  // Alternatively, you can wait until the page reaches a state where all cookies are set.
   console.log(page.url());
   // await expect(page.locator('h2:text("Menu")')).toBeVisible();
   await page.waitForLoadState('networkidle');
