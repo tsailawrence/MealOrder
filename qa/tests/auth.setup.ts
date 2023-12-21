b14eab38b9687a397646aed3c77d64071f359964
tsailawrence <tsailawrence812@gmail.com>
2023-12-21 08:38:17 +0800

    give up

--- qa/tests/auth.setup.ts
+++ qa/tests/auth.setup.ts
@@ -7,7 +7,6 @@ const screenshotsDir = path.join(__dirname, 'screenshots');
 if (!fs.existsSync(screenshotsDir)) {
   fs.mkdirSync(screenshotsDir, { recursive: true });
 }
-
 const baseURL = 'http://localhost:3000';
 const customerAccount = 'customer+clerk_test@test.com';
 const customerPassword = 'Customertest';
@@ -21,16 +20,16 @@ setup('authenticate as customer', async ({ page }) => {
   await page.goto(baseURL);
   console.log(page.url());
   await page.locator('span:text("Get Started")').click();
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(1000);
   await page.screenshot({ path: path.join(screenshotsDir, 'screenshot1.png') });
   console.log(page.url());
   await page.locator('#identifier-field').fill(customerAccount);
   await page.keyboard.press('Enter');
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(1000);
   console.log(page.url());
   await page.locator('#password-field').fill(customerPassword);
   await page.keyboard.press('Enter');
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(3000);
   // Wait until the page receives the cookies.
   // Alternatively, you can wait until the page reaches a state where all cookies are set.
   console.log(page.url());
@@ -48,16 +47,16 @@ setup('authenticate as merchant', async ({ page }) => {
   await page.goto(baseURL);
   console.log(page.url());
   await page.locator('span:text("Get Started")').click();
-  await page.waitForTimeout(8000);
+  await page.waitForTimeout(1000);
   console.log(page.url());
   await page.screenshot({ path: path.join(screenshotsDir, 'screenshot2.png') });
   await page.locator('#identifier-field').fill(merchantAccount);
   await page.keyboard.press('Enter');
-  await page.waitForTimeout(8000);
+  await page.waitForTimeout(1000);
   console.log(page.url());
   await page.locator('#password-field').fill(merchantPassword);
   await page.keyboard.press('Enter');
-  await page.waitForTimeout(8000);
+  await page.waitForTimeout(3000);
   // Wait until the page receives the cookies.
   // Alternatively, you can wait until the page reaches a state where all cookies are set.
   console.log(page.url());
