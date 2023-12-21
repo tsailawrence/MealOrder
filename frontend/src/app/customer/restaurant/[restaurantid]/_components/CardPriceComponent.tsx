9562a8b835be4f5d3daee9b4986246a0f755fd71
tsailawrence <tsailawrence812@gmail.com>
2023-12-21 12:02:31 +0800

    update

--- frontend/src/app/customer/restaurant/[restaurantid]/_components/CardPriceComponent.tsx
+++ frontend/src/app/customer/restaurant/[restaurantid]/_components/CardPriceComponent.tsx
@@ -1,215 +1,231 @@
-'use client'
+'use client';
 import React, { useState, FormEvent } from 'react';
 import Image from 'next/image';
-import { Textarea } from "@/components/ui/textarea"
+import { Textarea } from '@/components/ui/textarea';
 import SpecialInstruction from '@/components/SpecialInstruction';
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog";
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Skeleton } from '@/components/ui/skeleton';
 import toast from 'react-hot-toast';
 
 type Item = {
-    menuId: number;
-    name: string;
-    price: number;
-    quantity: number;
-    note?: string;
-    specialInstructions?: string[];
-}
+  menuId: number;
+  name: string;
+  price: number;
+  quantity: number;
+  note?: string;
+  specialInstructions?: string[];
+};
 type Cart = {
-    restaurantName: string;
-    storeId: number;
-    items: Item[]; //item name, price, quantity
-}
+  restaurantName: string;
+  storeId: number;
+  items: Item[]; //item name, price, quantity
+};
 type CardPriceProps = {
-    amount: number;
-    storeId: number;
-    restaurantName: string;
-    uri: string;
-    name: string;
-    menuId: number;
-    price: number;
-    description: string;
-    specialInstructions?: SpecialInstruction[];
-}
-export const CardPriceComponent: React.FC<CardPriceProps> = ({ amount, storeId ,restaurantName, uri, name, menuId , price, description }) => {
-    const [itemQuantity, setitemQuantity] = useState(1);
-    // const [isOptionSelected, setIsOptionSelected] = useState(true);
-    // const [selectedOption, setSelectedOption] = useState<string[]>([]);
-    const [textAreaValue, setTextAreaValue] = useState('');
-    const handleDecrease = () => {
-        if (itemQuantity >= 1) setitemQuantity(itemQuantity - 1);
-    }
+  amount: number;
+  storeId: number;
+  restaurantName: string;
+  uri: string;
+  name: string;
+  menuId: number;
+  price: number;
+  description: string;
+  specialInstructions?: SpecialInstruction[];
+};
+export const CardPriceComponent: React.FC<CardPriceProps> = ({
+  amount,
+  storeId,
+  restaurantName,
+  uri,
+  name,
+  menuId,
+  price,
+  description,
+}) => {
+  const [itemQuantity, setitemQuantity] = useState(1);
+  // const [isOptionSelected, setIsOptionSelected] = useState(true);
+  // const [selectedOption, setSelectedOption] = useState<string[]>([]);
+  const [textAreaValue, setTextAreaValue] = useState('');
+  const handleDecrease = () => {
+    if (itemQuantity >= 1) setitemQuantity(itemQuantity - 1);
+  };
 
-    const handleAdd = () => {
-        if (itemQuantity < amount)
-            setitemQuantity(itemQuantity + 1);
-    }
+  const handleAdd = () => {
+    if (itemQuantity < amount) setitemQuantity(itemQuantity + 1);
+  };
 
-    const handleTextAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setTextAreaValue(event.target.value);
+  const handleTextAreaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setTextAreaValue(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    addCart(); // Call the function to add item to cart
+    window.location.reload(); // Refresh the page
+  };
+
+  // const handleSelectionsChange = (selectedOptions: string[]) => {
+  //     // Process the selectedOptions array as needed
+  //     setSelectedOption(selectedOptions);
+  // };
+  const addCart = () => {
+    const newItem = {
+      menuId,
+      name,
+      price,
+      quantity: itemQuantity,
+      note: textAreaValue,
+      // specialInstructions: selectedOption || [],
     };
-
-    const handleSubmit = () => {
-        addCart(); // Call the function to add item to cart
-        window.location.reload(); // Refresh the page
+    if (itemQuantity > amount) {
+      alert('Please select a valid quantity');
+      return;
     }
-
-    // const handleSelectionsChange = (selectedOptions: string[]) => {
-    //     // Process the selectedOptions array as needed
-    //     setSelectedOption(selectedOptions);
-    // };
-    const addCart = () => {
-        const newItem = {
-            menuId,
-            name,
-            price,
-            quantity: itemQuantity,
-            note: textAreaValue,
-            // specialInstructions: selectedOption || [],
-        }
-        if (itemQuantity > amount) {
-            alert('Please select a valid quantity');
-            return;
-        }
-        console.log('newItem:', newItem);
-        // Retrieve existing cart data from local storage
-        let cart: Cart = JSON.parse(localStorage.getItem('cart') || '{}');
-        //cart storeid is different from current storeid or Empty, then clear cart
-        if (cart.storeId !== storeId || !cart.storeId) {
-            cart = {
-                restaurantName: restaurantName,
-                storeId: storeId,
-                items: []
-            };
-        }
-        //if have same item, then add quantity
-        let isSameItem = false;
-
-        cart.items.forEach((item) => {
-            if (item.menuId === newItem.menuId && item.note === newItem.note) {
-                item.quantity += newItem.quantity;
-                isSameItem = true;
-            }
-        });
-        if (!isSameItem) {
-            // Add new item to cart
-            cart.items.push(newItem);
-        } else {
-            isSameItem = false;
-        }
-
-        // Update local storage
-        localStorage.setItem('cart', JSON.stringify(cart));
-
-        console.log('Item added to cart:', newItem);
-        console.log('Updated cart:', cart.items);
-    }
+    console.log('newItem:', newItem);
+    // Retrieve existing cart data from local storage
     let cart: Cart = JSON.parse(localStorage.getItem('cart') || '{}');
-    const totalcost = cart.items?.reduce((acc, item) => acc + item.price * item.quantity, 0)
-    return (
-        <Dialog>
-            <DialogTrigger
-                className="flex flex-col items-center w-[95%] max-w-xs bg-white rounded-2xl overflow-hidden shadow-lg "
+    //cart storeid is different from current storeid or Empty, then clear cart
+    if (cart.storeId !== storeId || !cart.storeId) {
+      cart = {
+        restaurantName: restaurantName,
+        storeId: storeId,
+        items: [],
+      };
+    }
+    //if have same item, then add quantity
+    let isSameItem = false;
+
+    cart.items.forEach(item => {
+      if (item.menuId === newItem.menuId && item.note === newItem.note) {
+        item.quantity += newItem.quantity;
+        isSameItem = true;
+      }
+    });
+    if (!isSameItem) {
+      // Add new item to cart
+      cart.items.push(newItem);
+    } else {
+      isSameItem = false;
+    }
+
+    // Update local storage
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    console.log('Item added to cart:', newItem);
+    console.log('Updated cart:', cart.items);
+  };
+  let cart: Cart = JSON.parse(localStorage.getItem('cart') || '{}');
+  const totalcost = cart.items?.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+  return (
+    <Dialog>
+      <DialogTrigger className="flex flex-col items-center w-[95%] max-w-xs bg-white rounded-2xl overflow-hidden shadow-lg ">
+        <div className="flex flex-col items-center w-full max-w-xs bg-white rounded-2xl overflow-hidden shadow-lg max-md:w-full">
+          <div className="w-full">
+            <div
+              className="relative w-full h-0"
+              style={{ paddingBottom: '84%' }}
             >
-                <div
-                    className="flex flex-col items-center w-full max-w-xs bg-white rounded-2xl overflow-hidden shadow-lg max-md:w-full"
-                >
-                    <div className="w-full">
-                        <div className="relative w-full h-0" style={{ paddingBottom: '84%' }}>
-                            <Image
-                                src={uri}
-                                alt={name}
-                                width={86}
-                                height={86}
-                                className="absolute w-full h-full"
-                            />
-                        </div>
-                    </div>
-                    <div className="px-4 py-5 w-full">
-                        <h2 className="text-neutral-700 text-xl font-bold truncate">
-                            {name}
-                        </h2>
-                        <div className="flex items-center justify-between mt-3">
-                            <span className="text-red-600 text-xl">
-                                NT ${price}
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </DialogTrigger>
-            <DialogContent className='overflow-auto max-h-[90vh]'>
-                <div className="relative w-full h-0 " style={{ paddingBottom: '84%' }}>
-                    <Image
-                        src={uri}
-                        alt={name}
-                        width={86}
-                        height={86}
-                        className="absolute w-full h-full"
-                    />
-                </div>
-                <DialogHeader>
-                    <DialogTitle>
-                        <div className="text-neutral-400 text-base leading-6 self-stretch whitespace-nowrap max-md:max-w-full">
-                            {restaurantName}
-                        </div>
-                        <div className="justify-center text-neutral-950 text-2xl font-semibold leading-8 tracking-normal self-stretch mt-5 max-md:max-w-full">
-                            {name}
-                        </div>
-                    </DialogTitle>
-                    <DialogDescription>
-                        {description}
-                    </DialogDescription>
-                    {/* {specialInstructions && <SpecialInstruction specialInstructions={specialInstructions} onOptionChange={setIsOptionSelected} onSelectionsChange={handleSelectionsChange} />} */}
-                    <div className='pt-5 font-bold text-left'>note:</div>
-                    <Textarea value={textAreaValue} onChange={handleTextAreaChange} />
-                    <div className="self-stretch flex w-full items-center justify-between gap-5 mt-6 max-md:max-w-full max-md:flex-wrap">
-                        <div className="text-black text-3xl font-semibold leading-[50.4px] my-auto">
-                            NT${price * itemQuantity}
-                        </div>
-                        <div className="self-stretch flex items-center justify-between gap-5 max-md:justify-center">
-                            <Button
-                                className="items-stretch border self-stretch flex aspect-square flex-col p-3 rounded-lg mt-2"
-                                onClick={() => handleDecrease()}
-                                aria-label="Product Image"
-                            >
-                                -
-                            </Button>
-                            <div className="text-black text-center text-xl leading-6 tracking-tight opacity-[0.64] my-auto">
-                                {itemQuantity}
-                            </div>
-                            <Button
-                                className="items-stretch border self-stretch flex aspect-square flex-col p-3 rounded-lg mt-2"
-                                onClick={() => handleAdd()}
-                                aria-label="Product Image"
-                            >
-                                +
-                            </Button>
-                        </div>
-                    </div>
-                    <div className='text-neutral-400 text-end'>Remain Amount : {amount}</div>
-                    <div className='text-neutral-400 text-end'>Total Cost : {totalcost + price * itemQuantity}</div>
-                </DialogHeader>
-                <DialogFooter>
-                    <Button
-                        onClick={() => handleSubmit()}
-                        // disabled={!isOptionSelected || itemQuantity < 1}
-                        disabled={itemQuantity < 1}
-                        variant="destructive"
-                    >
-                        Add to Cart
-                    </Button>
-                </DialogFooter>
-                {itemQuantity < 1 && <div className='text-red-600 text-end'>Please select at least one item</div>}
-            </DialogContent>
-        </Dialog>
-    );
+              <Image
+                src={uri}
+                alt={name}
+                width={400}
+                height={400}
+                className="absolute w-full h-full"
+              />
+            </div>
+          </div>
+          <div className="px-4 py-5 w-full">
+            <h2 className="text-neutral-700 text-xl font-bold truncate">
+              {name}
+            </h2>
+            <div className="flex items-center justify-between mt-3">
+              <span className="text-red-600 text-xl">NT ${price}</span>
+            </div>
+          </div>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="overflow-auto max-h-[90vh]">
+        <div className="relative w-full h-0 " style={{ paddingBottom: '84%' }}>
+          <Image
+            src={uri}
+            alt={name}
+            width={400}
+            height={400}
+            className="absolute w-full h-full"
+          />
+        </div>
+        <DialogHeader>
+          <DialogTitle>
+            <div className="text-neutral-400 text-base leading-6 self-stretch whitespace-nowrap max-md:max-w-full">
+              {restaurantName}
+            </div>
+            <div className="justify-center text-neutral-950 text-2xl font-semibold leading-8 tracking-normal self-stretch mt-5 max-md:max-w-full">
+              {name}
+            </div>
+          </DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+          {/* {specialInstructions && <SpecialInstruction specialInstructions={specialInstructions} onOptionChange={setIsOptionSelected} onSelectionsChange={handleSelectionsChange} />} */}
+          <div className="pt-5 font-bold text-left">note:</div>
+          <Textarea value={textAreaValue} onChange={handleTextAreaChange} />
+          <div className="self-stretch flex w-full items-center justify-between gap-5 mt-6 max-md:max-w-full max-md:flex-wrap">
+            <div className="text-black text-3xl font-semibold leading-[50.4px] my-auto">
+              NT${price * itemQuantity}
+            </div>
+            <div className="self-stretch flex items-center justify-between gap-5 max-md:justify-center">
+              <Button
+                className="items-stretch border self-stretch flex aspect-square flex-col p-3 rounded-lg mt-2"
+                onClick={() => handleDecrease()}
+                aria-label="Product Image"
+              >
+                -
+              </Button>
+              <div className="text-black text-center text-xl leading-6 tracking-tight opacity-[0.64] my-auto">
+                {itemQuantity}
+              </div>
+              <Button
+                className="items-stretch border self-stretch flex aspect-square flex-col p-3 rounded-lg mt-2"
+                onClick={() => handleAdd()}
+                aria-label="Product Image"
+              >
+                +
+              </Button>
+            </div>
+          </div>
+          <div className="text-neutral-400 text-end">
+            Remain Amount : {amount}
+          </div>
+          <div className="text-neutral-400 text-end">
+            Total Cost : {totalcost + price * itemQuantity}
+          </div>
+        </DialogHeader>
+        <DialogFooter>
+          <Button
+            onClick={() => handleSubmit()}
+            // disabled={!isOptionSelected || itemQuantity < 1}
+            disabled={itemQuantity < 1}
+            variant="destructive"
+          >
+            Add to Cart
+          </Button>
+        </DialogFooter>
+        {itemQuantity < 1 && (
+          <div className="text-red-600 text-end">
+            Please select at least one item
+          </div>
+        )}
+      </DialogContent>
+    </Dialog>
+  );
 };
